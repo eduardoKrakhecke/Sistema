e4d1348aafca7a0b96912dbe26f48c06fb13127b
Eduardo\Eduardo Krakhecke <eduardo.godoi@digifred.com.br>
2018-09-05 22:02:14 -0300

    Implementação paginação, ok tabela,    ok tradução   ok busca tabela

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -9,3 +9,16 @@ app.config(function($routeProvider, $locationProvider){
     $locationProvider.html5Mode(false);
 });
 
+app.config(function($provide) {
+    $provide.decorator('GridOptions', ['$delegate', 'i18nService', function ($delegate, i18nService) {
+        var gridOptions;
+        gridOptions = angular.copy($delegate);
+        gridOptions.initialize = function (options) {
+            var initOptions;
+            initOptions = $delegate.initialize(options);
+            return initOptions;
+        };
+        i18nService.setCurrentLang('pt-br');
+        return gridOptions;
+    }]);
+});

be3dff207e3084e8422591b20dd79494caefe537
Eduardo\Eduardo Krakhecke <eduardo.godoi@digifred.com.br>
2018-09-10 22:07:44 -0300

    Mudança no table. Em implementação. Ajuste paginação Ajuste layout. Filtros

--- src/main/resources/static/module/listaProdutoComponent/listaProdutoController.js
+++ src/main/resources/static/module/listaProdutoComponent/listaProdutoController.js
@@ -4,20 +4,46 @@ app.controller("listaProdutoController", function ($scope, $http, $location, $q,
     vm.produto={};
     vm.produtos=[];
     vm.tituloPanel="Produtos";
+    vm.quantidaDeRegistrosPorPagina="5";
+    vm.pagina = 0;
+    vm.filtro = "";
+    vm.quantidadeDeRegistros = 0;
+    vm.ultimaPagina = false;
+    vm.primeiraPagina = false;
+    vm.ultimoRegistroDaPagina = 0;
 
-    vm.pageNumber=0;
-    vm.pageSize=10;
-    vm.filtro="";
+
+
+    vm.proximaPagina = function() {
+        vm.pagina++;
+        vm.carregarProdutos();
+    };
+
+    vm.paginaAnterior = function() {
+        vm.pagina--;
+        vm.carregarProdutos();
+    };
 
     vm.filtrar = function(){
         vm.carregarProdutos();
     };
 
-    vm.carregarProdutos=function(){
-        produto.getProdutoPaginado(vm.pageNumber, vm.pageSize, vm.filtro ).then(function (retorno) {
+    vm.carregarProdutos=function(zerarPaginacao){
+        if (zerarPaginacao){
+            vm.pagina = 0;
+        }
+        produto.getProdutoPaginado(vm.pagina, parseInt(vm.quantidaDeRegistrosPorPagina), vm.filtro ).then(function (retorno) {
             vm.produtos = retorno.conteudo;
+            vm.quantidadeDeRegistros = retorno.quantidadeDeRegistros;
+            vm.ultimaPagina = retorno.ultimaPagina;
+            vm.primeiraPagina = retorno.primeiraPagina;
+            vm.ultimoRegistroDaPagina = vm.pagina * vm.quantidadeDeRegistros + parseInt(vm.quantidaDeRegistrosPorPagina);
+            if (vm.ultimoRegistroDaPagina > vm.quantidadeDeRegistros) {
+                vm.ultimoRegistroDaPagina = vm.quantidadeDeRegistros;
+            }
         });
     };
 
+    vm.carregarProdutos();
 
 });
